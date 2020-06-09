7303ec9481f36e2a8bc1dc7db43fe4887a4df892
rakbarca10 <56740995+rakbarca10@users.noreply.github.com>
2020-06-09 14:17:51 +0530

    Update Winwheel.js

--- Winwheel.js
+++ Winwheel.js
@@ -1,7 +1,5 @@
 /*
-    Winwheel.js, by Douglas McKechie @ www.dougtesting.net
-    See website for tutorials and other documentation.
-
+   
     The MIT License (MIT)
 
     Copyright (c) 2018 Douglas McKechie
@@ -11,18 +9,9 @@
     in the Software without restriction, including without limitation the rights
     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
     copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
+    furnished to do so.
 
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-    SOFTWARE.
+   MINOR UPDATES ON 07-06-2020 BY RAKSHITH, CREDITS TO THE INITIAL DEVELOPER.
 */
 
 // ====================================================================================================================
